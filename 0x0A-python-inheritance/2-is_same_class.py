5cf1ca0a40a7448e78f6d1fe22eb67d2b17b7bab
AdexDaba <adexdaba23@gmail.com>
2022-08-01 06:19:06 -0700

    2-is_same_class.py

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
-"""Contains a single function"""
+"""
+returns True if the object is exactly an instance
+of the specified class ;otherwise False
+"""
 
 
 def is_same_class(obj, a_class):
+    """implementation
+    Args:
+        obj (Any): object to check
+        a_class (type): type to check against
+    Returns:
+        [boolean]: response
     """
-        Check if an object is an instance of the specified class
-    """
-
-    return (type(obj) == a_class)
+    return type(obj) == a_class
